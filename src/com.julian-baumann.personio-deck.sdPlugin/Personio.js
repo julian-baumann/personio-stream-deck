2cb0bbdd8f112b8f71dec440a7d365977270fb32
Julian Baumann <git@julba.de>
2024-03-22 16:13:02 +0100

    Initial commit

---
+++ src/com.julian-baumann.personio-deck.sdPlugin/Personio.js
@@ -0,0 +1,9 @@
+class PersonioSDK {
+    clientId;
+    clientSecret;
+
+    constructor(clientId, clientSecret) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret
+    }
+}
